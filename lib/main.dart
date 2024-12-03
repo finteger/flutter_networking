2f74da22195b580082f8775eaba59c0953e99a5a
finteger <nashassociatesinc@gmail.com>
2024-12-03 14:41:37 -0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MainApp());
+}
+
+Future<List<Map<String, dynamic>>> fetchPosts() async {
+  var response = await http.get(
+    Uri.parse('https://discuss.flarum.org/api/posts'),
+  );
+
+  if (response.statusCode == 200) {
+    //Parse the JSON response
+    Map<String, dynamic> data = json.decode(response.body);
+    //Extract the 'posts' data from the response
+    List posts = data['data'];
+
+    return List<Map<String, dynamic>>.from(posts);
+  } else {
+    throw Exception('Failed to load posts: ${response.statusCode}');
+  }
 }
 
 class MainApp extends StatelessWidget {
@@ -13,9 +31,18 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Placeholder(),
+          child: NetworkData(),
         ),
       ),
     );
+  }
+}
+
+class NetworkData extends StatelessWidget {
+  const NetworkData({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
